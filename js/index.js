cc3f92d7f783ffaebcc9a374abc71a682809dc8c
Mohsinali701701 <mohsinilyas6683@gmail.com>
2023-02-28 23:47:44 +0500

    Mega World Landing Page Completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-console.log('hekk');
+$(document).ready(function () {
+    $('#slot2').hide();
+  $('#load_more').on('click', function () {
+    $('#slot2').show();
+    $('#load_more').replaceWith(`<button type="button" class="btn nav_button text-white load_more" id="load_less"><div class="d-flex justify-content-center"><p>Show Less</p><i class='bx bxs-down-arrow-alt text-white fs-2'></i></div></button>`);
+  })
+  });
